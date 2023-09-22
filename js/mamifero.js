b3741ba637751f6ed501fef32b9d37c4d8094242
Aluno <aluno@edu.senac.br>
2023-09-22 20:49:29 -0300

    foi

---
+++ js/mamifero.js
@@ -0,0 +1,45 @@
+function andarPraFrente(){
+
+}
+function andarPraDireita(){
+
+}
+function andarPraEsquerda(){
+
+}
+function andarPraTras(){
+}
+
+function correr(){
+
+}
+function morder(){
+
+
+
+
+//se for 1 - o inimigo perde energia
+//se for 2 - arranca o pé do inimigo
+//se for 3 - arranca a perna
+//se for 4 - arranca os braços
+//se for 5- arranca a cabeça
+
+// vou criar uma variavel usando o let
+// usando o objeto document representa toda pagina
+let forca = document.querySelector("#forca").value
+let mensagem = document.querySelector("p")
+if(força==1){
+    mensagem.innerHTML = "inimigo perdeu energia!"
+}else if(força==2){
+    mensagem.innerHTML = "monstro zumbi ficou sem os pés"
+}else if(força==3)   
+    mensagem.innerHTML = "monstro zumbi sem as pernas"
+}else if(força==4) 
+    mensagem.innerHTML = "inimigo ficou sem os braços"  
+}else if(força==5)      
+
+
+
+
+
+}
